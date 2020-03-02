1918183ac861a8f38e8dff2994cd6b8d4d8672ce
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-03-02 15:53:36 -0500

    Add files via upload

--- ex1.asm
+++ ex1.asm
@@ -1,28 +1,14 @@
-
 .data
-  AA:     .space 400  		# int AA[100]
-  BB:     .space 400  		# int BB[100]
-  CC:     .space 400  		# int CC[100]
-  m:      .space 4   		# m is an int whose value is at most 10
-                     		# actual size of the above matrices is mxm
-
-      # You may add more variables here if you need to
-
-
-
+  AA:     .space 400        # int AA[100]
+  BB:     .space 400        # int BB[100]
+  CC:     .space 400        # int CC[100]
+   m:      .space 4 	    # m is an int which is the size of an mxm matrix
+ newline: .asciiz          "\n"
+ space:	  .asciiz	   " "	
 .text
 
 main:
 
-
-#------- INSERT YOUR CODE HERE for main -------
-#
-#  Best is to convert the C program line by line
-#    into its assembly equivalent.  Carefully review
-#    the coding templates near the end of Lecture 8.
-#
-#
-#  1.  First, read m (the matrices will then be size mxm).
   sw    $0,m($0)		#set m to 0
   add 	$8,$0,$0		# $8  to r (x)
   add 	$9,$0,$0		# $9  to c (y)
@@ -35,15 +21,15 @@ main:
   
   add	$10, $0, $0		#$10 is going to be the sum register
   
-for_outer_A:
-  beqz	$9, for_inner_A
+forrA:
+  beqz	$9, forcA  
   addi	$8, $8, 1
   beq	$8, $7, reset1
   add	$9, $0, $0		#set c to 0
-  j	for_inner_A
+  j	forcA
   
-for_inner_A:
-  beq	$9, $7, for_outer_A
+forcA:
+  beq	$9, $7, forrA
   addi	$v0, $0, 5		# system call 5 is for reading an integer
   syscall 			# integer value read is in $v0
   mult	$7, $8			#multiply m * r
@@ -52,22 +38,22 @@ for_inner_A:
   sll	$4, $4, 2		#offset
   sw	$v0, AA($4)		#store
   addi	$9, $9, 1
-  j	for_inner_A
+  j	forcA
   
 reset1:
   add 	$8,$0,$0		# r to 0
   add 	$9,$0,$0		# c to 0
   j	forrB
   
-for_outer_B: 
-  beqz	$9, for_inner_B
+forrB: 
+  beqz	$9, forcB 
   addi	$8, $8, 1
   beq	$8, $7, reset2 
   add	$9, $0, $0		#set c to 0
-  j	for_inner_B
+  j	forcB
  
-for_inner_B:
-  beq	$9, $7, for_outer_B
+forcB:
+  beq	$9, $7, forrB
   addi	$v0, $0, 5		# system call 5 is for reading an integer
   syscall 			# integer value read is in $v0
   mult	$7, $8			#multiply m * r
@@ -76,7 +62,7 @@ for_inner_B:
   sll	$4, $4, 2		#offset
   sw	$v0, BB($4)		#store
   addi	$9, $9, 1
-  j	for_inner_B
+  j	forcB
 
 reset2:
   add 	$8,$0,$0		# r to 0
@@ -154,30 +140,7 @@ printc:
   syscall           		# print the string
   addi	$9, $9, 1
   j	printc	
-	
-#BB will look the same	
-#CC will have outer, inner, middle loop
-
-#  2.  Next, read matrix A followed by matrix B.
-#  3.  Compute matrix product.  You will need triple-nested loops for this.
-#  4.  Print the result, one row per line, with one (or more) space(s) between
-#      values within a row.
-#  5.  Exit.
-#
-#------------ END CODE ---------------
-
 
 exit:                     # this is code to terminate the program -- don't mess with this!
   addi $v0, $0, 10      	# system call code 10 for exit
   syscall               	# exit the program
-
-
-
-#------- If you decide to make other functions, place their code here -------
-#
-#  You do not have to use helper methods, but you may if you would like to.
-#  If you do use them, be sure to do all the proper stack management.
-#  For this exercise though, it is easy enough to write all your code
-#  within main.
-#
-#------------ END CODE ---------------
